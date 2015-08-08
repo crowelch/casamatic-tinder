667b71e471271b5e62dc73d47db8c30157930e34
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-08-08 13:36:40 -0400

    missing curly racket

--- controllers/api.js
+++ controllers/api.js
@@ -696,47 +696,48 @@ exports.getLob = function (req, res, next) {
 
 exports.makeDecision = function (req, res) {
     if (req.user) {
-        User.findById(req.user.id, function(err, user) {
+        User.findById(req.user.id, function (err, user) {
             if (err) return console.log(err);
             if (req.body.yesDecision) {
                 user.profile.yesVotes.push(req.body.propertyId);
             } else {
                 user.profile.noVotes.push(req.body.propertyId);
             }
-            user.save(function(err) {
+            user.save(function (err) {
                 if (err) return console.log(err);
                 res.send('Kittens');
             });
         });
-}
-
-exports.getHouses = function (req, res, next) {
-    var user = req.user;
-    var options;
-    if (!user) {
-        options = {
-            minPrice: 100000,
-            maxPrice: 200000,
-            milesRadius: 5,
-            centerZip: 45219,
-            minBedrooms: 3,
-            minBathrooms: 2
-        };
-    } else {
-        var profile = user.profile;
-        options = {
-            minPrice: profile.minimumPrice,
-            maxPrice: profile.maximumPrice,
-            milesRadius: profile.maximumDistance,
-            centerZip: profile.location,
-            minBedrooms: profile.minimumNumBedrooms,
-            minBathrooms: profile.minimumNumBathrooms
-        };
     }
 
-    housesFromDb(options, function (housesCursor) {
-        housesCursor.toArray(function (err, houses) {
-            res.send(houses);
+    exports.getHouses = function (req, res, next) {
+        var user = req.user;
+        var options;
+        if (!user) {
+            options = {
+                minPrice: 100000,
+                maxPrice: 200000,
+                milesRadius: 5,
+                centerZip: 45219,
+                minBedrooms: 3,
+                minBathrooms: 2
+            };
+        } else {
+            var profile = user.profile;
+            options = {
+                minPrice: profile.minimumPrice,
+                maxPrice: profile.maximumPrice,
+                milesRadius: profile.maximumDistance,
+                centerZip: profile.location,
+                minBedrooms: profile.minimumNumBedrooms,
+                minBathrooms: profile.minimumNumBathrooms
+            };
+        }
+
+        housesFromDb(options, function (housesCursor) {
+            housesCursor.toArray(function (err, houses) {
+                res.send(houses);
+            });
         });
-    });
+    }
 }
