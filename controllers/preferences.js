c589bb192e54ab6a42c0425593b2d9eb0ea40487
probackpacker <probackpacker@gmail.com>
2015-08-08 03:43:32 -0400

    created get-houses

--- controllers/preferences.js
+++ controllers/preferences.js
@@ -1,30 +1,6 @@
-var geolib = require('geolib');
-var myJson = require('../secretJson/listings.json');
-var _ = require('lodash');
-var zipsMap = require('./zip_locations.json')
 
 exports.preferences = function (req, res) {
-	filter();
 	res.render('preferences', {
 		title: 'How much will you pay?'
 	});
-}
-
-function filter() {
-	var minPrice = 28000;
-	var maxPrice = 150000;
-	var minBedrooms = 2;
-	var minBathrooms = 1;
-	var startTime = Date.now();
-
-    var myHouses = myJson.filter(function (house) {
-        return (minPrice <= house.ListPrice.Value) 
-            && (maxPrice >= house.ListPrice.Value) 
-            && (minBedrooms <= house.Bedrooms) 
-            && (minBathrooms <= house.FullBathrooms);
-            ////&& geolib.isPointInCircle(houseLocation, userLocation, userRadius);
-	});
-	console.log(myJson.length);
-	console.log(myHouses.length);
-	console.log(Date.now() - startTime);
 }
---
+++ get-houses.js
@@ -0,0 +1,28 @@
+﻿var geolib = require('geolib');
+var housesArray = require('../secretJson/listings.json');
+var zipsMap = require('./zip_locations.json')
+
+
+function getLocationByZip(zipCode) {
+    return zipsMap[zipCode];
+}
+
+function milesToMeters(miles) {
+    return miles * 1609.34;
+}
+
+function getHouses(options) {
+    var centerLocation = getLocationByZip(options.centerZip);
+    var radiusMeters = milesToMeters(options.milesRadius);
+    var matchingHouses = housesArray.filter(function (house) {
+        var houseLocation = { latitude: house.Location.Latitude, longitude: house.Location.Longitude };
+        return (options.minPrice <= house.ListPrice.Value) 
+            && (options.maxPrice >= house.ListPrice.Value) 
+            && (options.minBedrooms <= house.Bedrooms) 
+            && (options.minBathrooms <= house.FullBathrooms) 
+            && geolib.isPointInCircle(houseLocation, centerLocation, radiusMeters);
+    });
+    return matchingHouses.slice(0, 25);
+}
+
+module.exports = getHouses;
