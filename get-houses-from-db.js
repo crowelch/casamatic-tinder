760ecfcec23c6c570bd3652b5ce5b05fe1503650
probackpacker <probackpacker@gmail.com>
2015-08-08 10:02:56 -0400

    Put the house list in Mongo and created a query to retrieve the matching houses.

---
+++ get-houses-from-db.js
@@ -0,0 +1,47 @@
+﻿//var zipsMap = require('./zip_locations.json');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+function getLocationByZip(zipCode) {
+    return zipsMap[zipCode];
+}
+
+function milesToMeters(miles) {
+    return miles * 1609.34;
+}
+
+function getHouses(options, callback) {
+    ////var centerLocation = getLocationByZip(options.centerZip);
+    ////var geoCenter = {
+    ////    type : "Point",
+    ////    coordinates : [parseFloat(centerLocation.Longitude) , parseFloat(centerLocation.Latitude)]
+    ////};
+    var radiusMeters = milesToMeters(options.milesRadius);
+    MongoClient.connect(url, function (err, database) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        var houses = database.collection('houses');
+        var matchingHouses = houses.find({
+            listPriceValue : { $gte : options.minPrice, $lte : options.maxPrice },
+            Bedrooms: { $gte: options.minBedrooms },
+            FullBathrooms: { $gte: options.minBathrooms },
+            loc : {
+                $near: {
+                    $geometry: {
+                        type: "Point" ,
+                        coordinates: [-84.3152 , 39.344258]
+                    },
+                    $maxDistance: radiusMeters
+                }
+            }
+        });
+        ////var matchingHouses = houses.find();
+        callback(matchingHouses.limit(25));
+
+    });
+}
+
+module.exports = getHouses;
---
+++ insert-houses.js
@@ -0,0 +1,33 @@
+﻿var housesArray = require('./secretJson/listings.json');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+
+var insertDocuments = function (db, callback) {
+    housesArray.forEach(function (house) {
+        house.loc = {
+            type : "Point",
+            coordinates : [parseFloat(house.Location.Longitude) , parseFloat(house.Location.Latitude)]
+        }
+        house.listPriceValue = parseInt(house.ListPrice.Value)
+    });
+
+    // Get the documents collection
+    var collection = db.collection('houses');
+    // Insert some documents
+    collection.insert(housesArray, function (err, result) {
+        assert.equal(err, null);
+        callback(result);
+    });
+}
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    insertDocuments(db, function () {
+        db.close();
+    });
+});
