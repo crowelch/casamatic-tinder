5b3b8152aad143d8813f3fa044b5203517ddd4a4
probackpacker <probackpacker@gmail.com>
2015-08-08 03:00:49 -0400

    Fix "requires"

--- controllers/preferences.js
+++ controllers/preferences.js
@@ -1,43 +1,30 @@
-var zippity = require('zippity-do-dah');
 var geolib = require('geolib');
-var firebase = require('firebase');
-var myFirebase = new firebase('https://casmastic.firebaseio.com/');
 var myJson = require('../secretJson/listings.json');
 var _ = require('lodash');
+var zipsMap = require('./zip_locations.json')
 
-exports.preferences = function(req, res) {
-	filter();
-	res.render('preferences', {
-		title: 'How much will you pay?'
-	});
+exports.preferences = function (req, res) {
+    filter();
+    res.render('preferences', {
+        title: 'How much will you pay?'
+    });
 }
 
 function filter() {
-	var minPrice = 28000;
-	var maxPrice = 150000;
-	var minBedrooms = 2;
-	var minBathrooms = 1;
-	var startTime = Date.now();
-	var myHouses = [];
-	myJson.map(function(house) {
-		if(minPrice <= house.ListPrice.Value) {
-			if(maxPrice >= house.ListPrice.Value) {
-				if(minBedrooms <= house.Bedrooms) {
-					if(minBathrooms <= house.FullBathrooms) {
-						// var distance = geolib.isPointInCircle(houseLocation, userLocation, userRadius);
-						// if(userDistance >= distance) {
-							myHouses.push(house);
-						// }
-					}
-				}
-			}
-		}
-	});
-	console.log(myJson.length);
-	console.log(myHouses.length);
-	console.log(Date.now() - startTime);
-}
+    var minPrice = 28000;
+    var maxPrice = 150000;
+    var minBedrooms = 2;
+    var minBathrooms = 1;
+    var startTime = Date.now();
 
-function zipMe(zipcode) {
-	return zippity.zipcode(zipcode)
+    var myHouses = myJson.filter(function (house) {
+        return (minPrice <= house.ListPrice.Value) 
+            && (maxPrice >= house.ListPrice.Value) 
+            && (minBedrooms <= house.Bedrooms) 
+            && (minBathrooms <= house.FullBathrooms);
+            ////&& geolib.isPointInCircle(houseLocation, userLocation, userRadius);
+    });
+    console.log(myJson.length);
+    console.log(myHouses.length);
+    console.log(Date.now() - startTime);
 }
