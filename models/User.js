32e2c75ca9f46a2d5ba4dfaf6f5f836a70fcbe3e
Kenneth Fechter <kenneth.fechter@gmail.com>
2015-08-07 21:06:58 -0400

    new schema

--- models/User.js
+++ models/User.js
@@ -3,23 +3,24 @@ var crypto = require('crypto');
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  email: { type: String, unique: true, lowercase: true },
-  password: String,
+    email: { type: String, unique: true, lowercase: true },
+    password: String,
 
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  tokens: Array,
+    facebook: String,
+    twitter: String,
+    google: String,
+    github: String,
+    instagram: String,
+    linkedin: String,
+    tokens: Array,
 
-  profile: {
-    name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' }
+    profile: {
+        name: { type: String, default: '' },
+        location: { type: String, default: '' },
+        minimumPrice: {type: String, default: ''},
+        maximumPrice: { type: String, default: '' },
+        minimumNumBed
+        picture: { type: String, default: '' }
   },
 
   resetPasswordToken: String,
